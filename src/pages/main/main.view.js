b1d1ea521e57f495a7e27eaa32bf52760e96139f
BubblyFace <751835145@qq.com>
2019-06-24 22:39:54 +0800

    feat: add sentence break in about area

--- src/pages/main/main.view.js
+++ src/pages/main/main.view.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { HeaderView as Header } from './view/header/header.view.js';
 import { MenuView as Menu } from './view/menu/menu.view.js';
 import { AboutView as About } from './view/about/about.view.js';
 
@@ -8,7 +7,6 @@ import { AboutView as About } from './view/about/about.view.js';
 export class MainView extends Component {
   render() {
     return <div>
-      <Header></Header>
       <Menu></Menu>
       <About></About>
     </div>
--- src/pages/main/view/about/about.view.js
+++ src/pages/main/view/about/about.view.js
@@ -1,9 +1,19 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { AUTHOR_INTRODUCE } from "../../../../../lib/const.js";
+import * as breakSentence from "../../../../../utils/break-sentence.js"
 
 
-export class AboutView extends Component {
-  render() {
-    return <div id="about">
-    </div>
-  }
+
+export function AboutView () {
+  const [ authorIntroduce ] = useState(AUTHOR_INTRODUCE);
+  let sentences = breakSentence.default(authorIntroduce, 2).map((sentence, index) => {
+    return <div class="about-sentence" key="{'sentence-' + index}">
+        {sentence}
+      </div>
+    })
+  return <div id="about">
+      {
+        sentences
+      }
+  </div>
 }
