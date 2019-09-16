f84ccd46c9456a017c0c35a095bd04b3a099cb17
BubblyFace <751835145@qq.com>
2019-09-16 21:28:31 +0800

    chore

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { MainView  } from './pages/main/main.view';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {MainView} from './pages/main/main.view';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
+/**
+ * @return {JSX} usersContent
+ */
 function Users() {
   return <h2>Users</h2>;
 }
 
 ReactDOM.render(
-  <Router>
-    <div>
+    <Router>
+      <div>
         <Route path="/" exact component={MainView} />
         <Route path="/users" component={Users} />
-    </div>
-  </Router>,
-  document.getElementById('root')
+      </div>
+    </Router>,
+    document.getElementById('root')
 );
--- src/pages/main/main.view.js
+++ src/pages/main/main.view.js
@@ -1,12 +1,13 @@
-import React, { Component } from "react";
-import { MenuView as Menu } from './view/menu/menu.view.js';
-import { AboutView as About } from './view/about/about.view.js';
+import React from 'react';
+import {MenuView as Menu} from './view/menu/menu.view.js';
+import {AboutView as About} from './view/about/about.view.js';
 
-export class MainView extends Component {
-  render() {
-    return <div>
-      <Menu></Menu>
-      <About></About>
-    </div>
-  }
+/**
+ * @return {JSX}
+ */
+export function MainView() {
+  return <div>
+    <Menu></Menu>
+    <About></About>
+  </div>;
 }
