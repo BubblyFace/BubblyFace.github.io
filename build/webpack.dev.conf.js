c6533525c1c8988dcd929324e2d5ff600f9dc570
浩维  孙 <haoweisun@haoweideMacBook-Pro.local>
2020-03-16 23:25:18 +0800

    fix:webpack-dev-server router refresh 404

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -6,6 +6,19 @@ module.exports = merge(baseConfig, {
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist',
-    port: 3000
+    port: 3000,
+    historyApiFallback: true
+    // proxy: {
+    //   '/': {
+    //     target: 'http://127.0.0.1:8000',
+    //     secure: false,
+    //     bypass: function(req, res, proxyOptions) {
+    //       if (req.headers.accept.indexOf('html') !== -1) {
+    //         console.log('Skipping proxy for browser request.');
+    //         return '/index.html';
+    //       }
+    //     }
+    //   }
+    // }
   }
 });
