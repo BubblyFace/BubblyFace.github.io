64fd724ef8b14caee0125c25abff96629e378da9
BubblyFace <751835145@qq.com>
2020-08-30 23:39:13 +0800

    chore

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { MainView } from './pages/main/main.view';
 import { ArticleView } from './pages/article/article.view';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 /**
  * @return {JSX} usersContent
  */
-
 
 ReactDOM.render(
   <Router>
     <div>
       <Route path="/" exact component={MainView} />
       <Route path="/article" component={ArticleView} />
-    </div>
+      <Link to="/article">阿哈哈哈哈</Link>
+    </div>  
   </Router>,
   document.getElementById('root')
 );
